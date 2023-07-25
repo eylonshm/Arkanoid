bd95025264124b88cc64a6c82de443319f6ffd49
Eylon Shmilovich <eylonshm@gmail.com>
2023-07-25 10:38:06 +0300

    Add Brick Class

--- src/sprites/Brick.ts
+++ src/sprites/Brick.ts
@@ -1,3 +1,33 @@
 import { Vector } from '../types'
 
-export class Brick {}
+export class Brick {
+  private brickImage: HTMLImageElement = new Image()
+
+  constructor(private brickWidth: number, private brickHeight: number, private brickPosition: Vector, private brickEnergy: number, image: string) {
+    this.brickWidth = brickWidth
+    this.brickHeight = brickHeight
+    this.brickPosition = brickPosition
+    this.brickEnergy = brickEnergy
+    this.brickImage.src = image
+  }
+
+  get width(): number {
+    return this.brickWidth
+  }
+
+  get height(): number {
+    return this.brickHeight
+  }
+
+  get position(): Vector {
+    return this.brickPosition
+  }
+
+  get energy(): number {
+    return this.brickEnergy
+  }
+
+  set energy(energy: number) {
+    this.brickEnergy = energy
+  }
+}
