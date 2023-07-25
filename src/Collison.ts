632075c8d4e5a2e00e0a88b4db07993ae44f9032
Eylon Shmilovich <eylonshm@gmail.com>
2023-07-25 13:24:06 +0300

    Add Collison class

---
+++ src/Collison.ts
@@ -0,0 +1,46 @@
+import { Brick, Ball, Paddle } from './sprites'
+import { CanvasView } from './view/CanvasView'
+
+export class Collison {
+  checkBrickCollison = (ball: Ball, brick: Brick): boolean => {
+    if (
+      ball.pos.x < brick.pos.x + brick.width &&
+      ball.pos.x + ball.width > brick.pos.x &&
+      ball.pos.y < brick.pos.y + brick.height &&
+      ball.pos.y + ball.height > brick.pos.y
+    ) {
+      return true
+    }
+    return false
+  }
+
+  isCollidingBricks = (ball: Ball, bricks: Brick[]): boolean => {
+    bricks.forEach((brick, i) => {
+      if (this.checkBrickCollison(ball, brick)) {
+        ball.changeYDirection()
+        if (brick.energy === 1) {
+          bricks.splice(i, 1)
+        } else {
+          brick.energy -= 1
+        }
+        return true
+      }
+    })
+    return false
+  }
+
+  isCollidingBall = (ball: Ball, paddle: Paddle, view: CanvasView): void => {
+    // 1. Check ball collision with paddle
+    if (ball.pos.x + ball.width > paddle.pos.x && ball.pos.x < paddle.pos.x + paddle.width && ball.pos.y + ball.height === paddle.pos.y) {
+      ball.changeYDirection()
+    }
+
+    // 2. Check ball collision with walls
+    if (ball.pos.x > view.canvas.width - ball.width || ball.pos.x < 0) {
+      ball.changeXDirection()
+    }
+    if (ball.pos.y < 0) {
+      ball.changeYDirection()
+    }
+  }
+}
